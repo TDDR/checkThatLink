511cebf2e30c284a8da9f28f6e77e0575f42a842
TIM <troberts10@myseneca.ca>
2020-10-06 20:16:45 -0400

    added -j option for JSON output

--- release_0.1/src/checkFile.py
+++ release_0.1/src/checkFile.py
@@ -13,20 +13,29 @@ class checkFile:
         self.style = colourText()
         self.timeout = urllib3.Timeout(connect=2.5, read=2.5,)
         self.manager = urllib3.PoolManager(timeout=self.timeout)
-        self.secureCheck = args
-        
+        self.secureCheck = args[0]
+        self.jsonOut = args[1]
+        self.json = []
+
         self.checkThatFile()
+
 
     def checkThatFile(self):
         print('Getting status of links...')
         for line in self.file:
             line = self.parseWebAddress(line)
-            self.printStatusCode(line)
-            
-            if(self.secureCheck):
-                self.secureHttpChecker(line)
-    
-    
+            if(not self.jsonOut):
+                self.printStatusCode(line)
+                if(self.secureCheck):
+                    self.secureHttpChecker(line)
+            else:
+                self.printStatusAsJSON(line)
+                if(self.secureCheck):
+                    self.secureHttpCheckerJSON(line)
+        if(self.jsonOut):
+            print(self.json)
+                
+        
     def printStatusCode(self, link):
             try:
                 response = self.manager.request('HEAD', link)
@@ -39,7 +48,17 @@ class checkFile:
             except Exception as e:
                 print(f"{self.style._unknownLink}[???] {link} {self.style._plainText}")
 
-            
+
+    def printStatusAsJSON(self, link):
+            try:
+                response = self.manager.request('HEAD', link)
+                status = response.status
+                self.json.append({"url": link, "status": status})
+
+            except Exception as e:
+                self.json.append({"url": link, "status": "???"})
+
+       
     def parseWebAddress(self,line):
         line = re.sub('<a href="', '', line)
         line = re.sub('">.*$[\r\n]', '', line)         
@@ -64,3 +83,18 @@ class checkFile:
                     print(f"{self.style._securedLink}[{status}] {link} {self.style._plainText}")
             except Exception as e:
                 pass
+
+
+    def secureHttpCheckerJSON(self, link):
+            isHttp = re.match('(http)', link)
+            
+            if(isHttp):
+                link = re.sub('(http)','https', link)
+                try:
+                    response = self.manager.request('HEAD', link)
+                    status = response.status
+
+                    if(status < 400):
+                        self.json.append({"url": link, "status": status})
+                except Exception as e:
+                    pass
