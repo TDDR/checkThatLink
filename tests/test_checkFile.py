a2a73c6528db456837853171d6e3d5c3058e2322
Roger Yu <roger.yu27@gmail.com>
2020-11-25 12:32:07 -0500

    Added more tests & fixed mock responses

--- tests/test_checkFile.py
+++ tests/test_checkFile.py
@@ -27,30 +27,59 @@ def test_no_file_exception():
     args.file = "resources/test.html"
 
 
-@mock.patch("src.checkFile.checkFile.headRequest")
-def test_headRequest_200(mock_headRequest):
+def mocked_requests_get(*args, **kwargs):
+    class MockResponse:
+        def __init__(self, status):
+            self.status = status
 
+        def status(self):
+            return self
+
+    if args[1] == "http://google.com":
+        return MockResponse(400)
+    elif args[1] == "http://google.cim":
+        return MockResponse(404)
+
+    return MockResponse("???")
+
+
+@mock.patch("urllib3.PoolManager.request", side_effect=mocked_requests_get)
+def test_headRequest_200(self):
     link = "http://google.com"
-    mock_headRequest.return_value = {"url": link, "status": 200, "secured": False}
     cF = checkFile.checkFile(args)
+    cF.headRequest(link)
 
-    assert cF.headRequest(link) == {
+    assert cF.allLinks[0] == {
         "url": "http://google.com",
-        "status": 200,
+        "status": 400,
         "secured": False,
     }
 
 
-@mock.patch("src.checkFile.checkFile.headRequest")
-def test_headRequest_404(mock_headRequest):
+@mock.patch("urllib3.PoolManager.request", side_effect=mocked_requests_get)
+def test_headRequest_404(self):
 
     link = "http://google.cim"
-    mock_headRequest.return_value = {"url": link, "status": 404, "secured": False}
     cF = checkFile.checkFile(args)
+    cF.headRequest(link)
 
-    assert cF.headRequest(link) == {
+    assert cF.allLinks[0] == {
         "url": "http://google.cim",
         "status": 404,
+        "secured": False,
+    }
+
+
+@mock.patch("urllib3.PoolManager.request", side_effect=mocked_requests_get)
+def test_headRequest_unknown(self):
+
+    link = ""
+    cF = checkFile.checkFile(args)
+    cF.headRequest(link)
+
+    assert cF.allLinks[0] == {
+        "url": "",
+        "status": "???",
         "secured": False,
     }
 
@@ -62,3 +91,34 @@ def test_parseWebAddress():
     cF = checkFile.checkFile(args)
 
     assert cF.parseWebAddress(lineToParse) == "https://www.google.com/search?q=help"
+
+
+@mock.patch("urllib3.PoolManager.request", side_effect=mocked_requests_get)
+def test_printAll(self, capsys):
+    link = "http://google.com"
+    cF = checkFile.checkFile(args)
+    cF.headRequest(link)
+    cF.printAll()
+    captured = capsys.readouterr()
+    assert captured.out == "\x1b[91m[400] http://google.com\x1b[0m\n"
+
+
+@mock.patch("urllib3.PoolManager.request", side_effect=mocked_requests_get)
+def test_printBadResultsJSON(self, capsys):
+    link = "thisdoesnotwork"
+    cF = checkFile.checkFile(args)
+    cF.headRequest(link)
+    cF.printAll()
+    captured = capsys.readouterr()
+    assert captured.out == "\x1b[90m[???] thisdoesnotwork\x1b[0m\n"
+
+
+@mock.patch("urllib3.PoolManager.request", side_effect=mocked_requests_get)
+def test_checkThatFile(self, capsys):
+    answer_str = "[{'url': 'http://google.com', 'status': 400}]\n"
+    link = "http://google.com"
+    cF = checkFile.checkFile(args)
+    cF.headRequest(link)
+    cF.printAllJSON()
+    captured = capsys.readouterr()
+    assert captured.out == answer_str
